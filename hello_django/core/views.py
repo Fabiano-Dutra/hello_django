7650c66dcdf2e9227051ad680b1c034ce134b089
Fabiano Dutra <fdutra.oliveira@gmail.com>
2020-06-16 17:24:02 -0300

    Incluindo excercício com as 4 operações matemáticas

--- hello_django/core/views.py
+++ hello_django/core/views.py
@@ -8,3 +8,17 @@ from django.shortcuts import render, HttpResponse
 def hello(request, nome, idade):
     # Retornará a mensagem abaixo usanto o interpretador 'HttpResponse' interpretar a imagem para http
     return HttpResponse(f'<h1>Hello {nome} de {idade} anos<h1>')
+
+# Criando método soma, que recebe 2 números e responde com sua soma
+def soma(request, n1, n2):
+    # Retornará o mensagem abaixo usando o interpretador 'HttpResponse'
+    return  HttpResponse(f'<h1>Soma de {n1} + {n2} = {n1 + n2}<h1>')
+
+def subtrai(request, n1, n2):
+    return HttpResponse(f'<h1>Subtração de {n1} - {n2} = {n1 - n2}<h1>')
+
+def multiplica(request, n1, n2):
+    return HttpResponse(f'<h1>Multiplicação de {n1} x {n2} = {n1 * n2}')
+
+def divide(request, n1, n2):
+    return HttpResponse(f'Divisão de {n1} / {n2} = {n1 / n2}')
--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -20,5 +20,10 @@ from Django_e_Python.hello_django.core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Incluí um novo caminho hello/ com seus parâmetros
-    path('hello/<nome>/<int:idade>/', views.hello)
+    path('hello/<nome>/<int:idade>/', views.hello),
+    # Exercício: criando 3 rotas/caminhos que fazer 4 operaçõs básicas
+    path('soma/<int:n1>/<int:n2>/', views.soma),
+    path('subtração/<int:n1/<int:n2>', views.subtrai),
+    path('multiplicação/<n1>/<n2>/', views.multiplica),
+    path('divisão/<n1>/<n2>/', views.divide)
 ]
